c996bd64909c0fe567d984c9f6e874e36f12d5e0
Alar-q <alar.akilbekov99@gmail.com>
2022-09-13 23:16:09 +0600

    com

--- hello_react/13_routing/server/index.js
+++ hello_react/13_routing/server/index.js
@@ -5,19 +5,23 @@ import fs from 'fs';
 import React from 'react';
 import ReactDOMServer from 'react-dom/server';
 
+import {Home, About, Events, Contact, Products} from '../client/components/pages.js';
+import Counter from '../client/components/Counter.js';
+
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+
+app.get('/counter', (req, res)=>{
+	res.sendFile(path.resolve(__dirname, '../build-client/index.html'));
+});
+
+app.get('/', (req, res)=>{
+	res.send(path.resolve(__dirname, '../build-client/index.html'));
+});
+
 app.use(express.static(path.resolve(__dirname, '../build-client')));
-
-app.get('/about', (req, res)=>{
-	res.sendFile(path.resolve(__dirname, '../build-client/index.html'));
-});
-
-app.get('*', (req, res)=>{
-	res.sendFile(path.resolve(__dirname, '../build-client/index.html'));
-});
 
 app.listen(PORT, ()=>{
 	console.log(`Server is listening on port ${PORT}`);
