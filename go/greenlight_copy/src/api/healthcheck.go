dc001051a031237ad0b91487a7aa451a1d5cdea7
Alar <alar.akilbekov99@gmail.com>
2023-02-02 21:37:00 +0600

    json response using Marshal

--- go/greenlight_copy/src/api/healthcheck.go
+++ go/greenlight_copy/src/api/healthcheck.go
@@ -1,16 +1,27 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
 	"net/http"
 )
 
 // (req, res) => {...}
 func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
-	js := `{"status": "available", "environment": %q, "version": %q}`
-	js = fmt.Sprintf(js, app.config.env, version)
+	data := map[string]string{
+		"status":      "available",
+		"environment": app.config.env,
+		"version":     version,
+	}
 
-	w.Header().Set("Content-Type", "application/json" /*; charset=utf-8 //not necessary */)
+	js, err := json.Marshal(data)
+	if err != nil {
+		app.logger.Print(err)
+		http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
+		return
+	}
 
-	w.Write([]byte(js))
+	js = append(js, '\n')
+	w.Header().Set("Content-Type", "application/json")
+
+	w.Write(js)
 }
