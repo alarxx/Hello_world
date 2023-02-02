616408d748b9106db63081f73ce201cd8992e628
Alar <alar.akilbekov99@gmail.com>
2023-02-02 22:15:44 +0600

    struct tags

--- go/greenlight_copy/internal/data/movies.go
+++ go/greenlight_copy/internal/data/movies.go
@@ -3,11 +3,11 @@ package data
 import "time"
 
 type Movie struct {
-	ID          int64
-	CreatedDate time.Time
-	Title       string
-	Year        int32
-	Runtime     int32
-	Genres      []string
-	Version     int32
+	ID          int64     `json:"id"` // struct tag
+	CreatedDate time.Time `json:"created_date"`
+	Title       string    `json:"title"`
+	Year        int32     `json:"year"`
+	Runtime     int32     `json:"runtime"`
+	Genres      []string  `json:"genres"`
+	Version     int32     `json:"version"`
 }
