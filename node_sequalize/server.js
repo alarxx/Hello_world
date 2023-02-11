09313bda2e67a66f0095a33b137b1474503334a3
unknown <alar.akilbekov99@gmail.com>
2023-02-11 10:50:55 +0600

    set server

--- node_sequalize/server.js
+++ node_sequalize/server.js
@@ -1,8 +1,42 @@
+const { credentials } = require(`./config`);
+
 const express = require('express');
 
 const app = express();
 
+const http = require('http');
+const server = http.createServer(app);
+
+/** Sessions */
+/*const expressSession = require('express-session');
+const expiresIn = 1000 * 60 * 60 * 24 * 7; // 7 days in milliseconds
+const sessionMiddleware = expressSession({
+    secret: credentials.cookieSecret,
+    cookie: {
+        maxAge: expiresIn,
+    },
+    // store: sessionStore, // connect-mongodb-session, например, или что похожая с Redis
+    resave: false,
+    saveUninitialized: false,
+});
+app.use(sessionMiddleware);*/
+
+
+/** Доступ к статическим файлам */
+app.use(express.static(__dirname + '/public'));
+
+/** Body Parser */
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+/** File Upload */
+const fileUpload = require('express-fileupload');
+app.use(fileUpload());
+
 app.use(require('./routes/root'));
 
 const port = process.env.PORT || 3000;
-app.listen(port, ()=>console.log(`Server is running on port ${port}`));
+server.listen(port, ()=>{
+    console.log(`Server is running on port ${port}`)
+});
