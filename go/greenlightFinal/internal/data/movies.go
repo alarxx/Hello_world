39e7ae13388d226e4a06350a8669a1471685a4f2
Alar <alar.akilbekov99@gmail.com>
2023-02-23 05:13:47 +0600

    sorting

--- go/greenlightFinal/internal/data/movies.go
+++ go/greenlightFinal/internal/data/movies.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"database/sql"
 	"errors"
+	"fmt"
 	"github.com/lib/pq"
 	"greenlight.alar.net/internal/validator"
 	"time"
@@ -179,12 +180,12 @@ func (m MovieModel) Delete(id int64) error {
 func (m *MovieModel) GetAll(title string, genres []string, filters Filters) ([]*Movie, error) {
 	// Before postgre full-text search LOWER(title) = LOWER($1)
 	// Also we have STRPOS() and ILIKE operators
-	query := `
-		SELECT id, created_at, title, year, runtime, genres, version 
-		FROM movies 
+	query := fmt.Sprintf(`
+		SELECT id, created_at, title, year, runtime, genres, version
+		FROM movies
 		WHERE (to_tsvector('english', title) @@ plainto_tsquery('english', $1) OR $1 = '')
 		AND (genres @> $2 OR $2 = '{}')
-		ORDER BY id`
+		ORDER BY %s %s, id ASC`, filters.sortColumn(), filters.sortDirection())
 
 	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
 	defer cancel()
