3c9e6c23a61d9e54eaafca5ba18941850127491e
Alar <alar.akilbekov99@gmail.com>
2023-03-17 03:32:40 +0600

    htmls

--- Node/NodePassportLocal/server.js
+++ Node/NodePassportLocal/server.js
@@ -1,15 +1,31 @@
 require('dotenv').config()
 
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
-app.use(express.json())
+/** JSON Parsing */
+// parse application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+// parse application/json
+app.use(express.json());
+
 const multer = require('multer')
 app.use(multer().any())
 
-app.get('/', (req, res)=>{
-    res.json({ page: 'home' });
+app.get('/login', (req, res)=>{
+    res.sendFile(path.join(__dirname, 'html', 'login.html'))
+})
+app.get('/register', (req, res)=>{
+    res.sendFile(path.join(__dirname, 'html', 'register.html'))
+})
+
+app.post('/login', (req, res)=>{
+    res.json(req.body)
+})
+app.post('/register', (req, res)=>{
+    res.json(req.body)
 })
 
 const PORT = process.env.PORT || 3000;
