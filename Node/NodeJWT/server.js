61c61d8c844b132a960b22d36db5fb2d6cc54100
Alar <alar.akilbekov99@gmail.com>
2023-03-16 07:25:55 +0600

    mongo

--- Node/NodeJWT/server.js
+++ Node/NodeJWT/server.js
@@ -1,14 +1,36 @@
 require('dotenv').config()
 
 const express = require('express');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const mongoose = require('mongoose')
 
 const app = express();
+
+app.use(express.json())
+app.use(cookieParser())
+app.use(cors())
 
 app.get('/', (req, res)=>{
     res.json({ page: 'home' });
 })
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, ()=>{
-    console.log(`server is listening on port ${PORT}`);
-})
+async function start(){
+    try{
+        await mongoose.connect(process.env.DB_DNS, {
+            useNewUrlParser: true,
+        }).then(() => {
+            console.log(`MongoDB connected`, `${process.env.DB_DNS}`);
+        }).catch(err => {
+            console.log('Failed to connect to MongoDB', err);
+        });
+
+        app.listen(PORT, ()=>{
+            console.log(`server is listening on port ${PORT}`);
+        })
+    }catch(e){
+        console.log(e)
+    }
+}
+start();
