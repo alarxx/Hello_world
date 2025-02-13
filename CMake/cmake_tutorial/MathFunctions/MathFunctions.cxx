fa1cf12e23dcb380f6d2ae66d31acd749fdafe84
Alar Akilbekov (Win) <alar.akilbekov@gmail.com>
2025-02-13 12:29:31 +0500

    refactor: test on windows

--- CMake/cmake_tutorial/MathFunctions/MathFunctions.cxx
+++ CMake/cmake_tutorial/MathFunctions/MathFunctions.cxx
@@ -24,3 +24,18 @@ namespace mathfunctions
     #endif
   }
 }
+
+/*
+Somehow on linux we can use this function with only one definition in tutorial.cxx
+*/
+void print_cxx_std(){
+// https://stackoverflow.com/questions/2324658/how-to-determine-the-version-of-the-c-standard-used-by-the-compiler
+  if (__cplusplus == 202101L) std::cout << "C++23";
+  else if (__cplusplus == 202002L) std::cout << "C++20";
+  else if (__cplusplus == 201703L) std::cout << "C++17";
+  else if (__cplusplus == 201402L) std::cout << "C++14";
+  else if (__cplusplus == 201103L) std::cout << "C++11";
+  else if (__cplusplus == 199711L) std::cout << "C++98";
+  else std::cout << "pre-standard C++." << __cplusplus;
+  std::cout << "\n";
+}
